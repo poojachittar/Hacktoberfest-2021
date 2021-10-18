cd9af83b7fe86ac8397fca190fc8bc7b10d9e11e
SrushtiBankar <88138554+SrushtiBankar@users.noreply.github.com>
2021-10-18 20:00:35 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+ 	int i;
+ 	int arr[10];
+ 	
+ 	printf("\nEnter any 10 nos: ");
+ 	
+ 	
+ 	for(i=0;i<10;i++)
+ 	{
+ 		scanf("%5d",&arr[i]);
+	 }
+	 printf("\n--------Array-------- ");
+	 for(i=0;i<10;i++)
+ 	{
+ 		printf("%5d",arr[i]);
+	}
+	printf("\nEven nos in array are: ");
+	for(i=0;i<10;i++)
+	{
+		if(arr[i]%2==0)
+		printf("%5d",arr[i]);
+			
+	}
+	printf("\nOdd nos in array are: ");	
+		for(i=0;i<10;i++)
+	{
+		if(arr[i]%2!=0)
+		printf("%5d",arr[i]);
+			
+	}
+}
+ 
